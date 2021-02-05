a2eaa404b29228917d562730260735d5b0d6d4af
Neeka Sewnath <nsewnath@ufl.edu>
2021-02-05 18:40:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ZooArchNet Data Cleaner
+Data cleaning web application for ZooArchNet
+
+How to Run: 
+1. Navigate to the project folder in Command Prompt/Terminal.
+2. Make sure you have all of the dependencies installed (namely Pandas and Flask)
+3. Run the python file [app.py](http://app.py) (you can use command python app.py).
+4. Navigate to [http://localhost:5000/](http://localhost:5000/).
+5. Select a csv or excel file you want to clean and selecting cleaning routine. 
+
---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask_cors import CORS
+from flask import request
+from flask import render_template, redirect, send_file
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('clean.html')
+
+@app.route('/clean', methods=['POST'])
+def clean_file():
+
+    verbatim_name = request.form.get('verbatim-name')
+
+    file = request.files['file']
+
+    mimetype = file.content_type
+    
+    if mimetype == 'text/csv' or mimetype == 'application/vnd.ms-excel':
+      df = pd.read_csv(file)
+    else:
+      df = pd.read_excel(file)
+
+    if verbatim_name:
+      df.dropna(inplace=True)
+
+    
+
+
+
+
+    
+      
+    redirect('http://localhost:5000')
+    if mimetype == 'text/csv' or mimetype == 'application/vnd.ms-excel':
+      df.to_csv('./data.csv')
+      return send_file('./data.csv',
+                     mimetype='text/csv',
+                     attachment_filename='data.csv',
+                     as_attachment=True)
+    else:
+      df.to_excel('./data.xlsx')
+      return send_file('./data.xlsx',
+                     mimetype='text/xlsx',
+                     attachment_filename='data.xlsx',
+                     as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
